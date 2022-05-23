38454dddcebff214ad705a1be7f34d1a95c3dc64
Valeriy Negriyko <admin@vaillant.ua>
2022-05-23 13:11:10 +0300

    1st lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import './App.css';
 import { Footer } from './Footer';
 
-const user = {username: "Valerii"};
+const user = {
+  firstname: "Valerii",
+  lastname: "Negriiko",
+  biography: "Born 1978, kindergardern, school, university, work. Married, two children",
+  address: {street: "Gradinskaya", house: 7, app: "13"},
+  contacts: {email: "negriyko@gmail.com", mob: "+380674055264"}
+};
 
 const App = () => (
   <div className="App">
-    <h1>Hello world {user.username}</h1>
+    <h1>Short info about {user.firstname} {user.lastname}</h1>
+    <h2>Biography</h2>
+    <p>{user.biography}</p>
+    <h2>Contacts</h2>
+    <p><b>address: </b>{user.address.street}, {user.address.house}, app. {user.address.app}</p>
+    <p><b>e-mail: </b>{user.contacts.email}</p>
+    <p><b>phone: </b>{user.contacts.mob}</p>
     <Footer copyright="C"/>
   </div>
 )
